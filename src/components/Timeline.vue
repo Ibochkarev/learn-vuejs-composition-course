bd2e81f27952b694ff40dbb810ae317d8bc234ad
Bochkarev Ivan <ivanx86@gmail.com>
2023-01-05 16:06:05 +0600

    first commit

---
+++ src/components/Timeline.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period in periods"
+        :key="period"
+        :class="{ 'is-active': period === currentPeriod }"
+        @click="setPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+type Period = 'Today' | 'This Week' | 'This Month'
+
+export default defineComponent({
+  name: 'Timeline',
+
+  setup() {
+    const periods = ['Today', 'This Week', 'This Month']
+    const currentPeriod = ref<Period>('Today')
+
+    const setPeriod = (period: Period) => {
+      currentPeriod.value = period
+    }
+
+    return {
+      periods,
+      currentPeriod,
+      setPeriod
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
